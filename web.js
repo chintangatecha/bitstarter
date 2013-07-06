1d71a933b1a7ef9ce493dfb8bcb1b40a5a5ea6d4
chintangatecha <chintangatecha@yahoo.co.in>
2013-07-06 15:55:37 +0000

    this is test again

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync(index.html);
+  fs = require('fs')
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
 });
 
 var port = process.env.PORT || 5000;
